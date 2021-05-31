c5a5fa3a1a9192ba93a521c3bd4917380b4a39b2
kirilinsky <kirill.ilinsky@gmail.com>
2021-06-01 00:26:50 +0300

    idk anything

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -2,16 +2,31 @@ const path = require('path');
 const Koa = require('koa');
 const app = new Koa();
 
+
 app.use(require('koa-static')(path.join(__dirname, 'public')));
 app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
 
+let stack = []
+
 router.get('/subscribe', async (ctx, next) => {
+    await new Promise(connect => {
+        stack.push(connect)
+    })
 });
 
 router.post('/publish', async (ctx, next) => {
+    let { message: msg } = ctx.request.body
+    stack.forEach((resolve) => {
+        resolve(msg)
+    })
+
+    stack = []
+
+    ctx.body = 'success'
+
 });
 
 app.use(router.routes());
