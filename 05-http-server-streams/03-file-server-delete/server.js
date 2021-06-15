74e7e27d47b0eb45feea3eb7220fd5951ebd9bd2
Kirill Ilinsky <kirill.ilinsky@gmail.com>
2021-06-15 14:11:00 +0300

    http delete method

--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const { unlink } = require('fs');
 
 const server = new http.Server();
 
@@ -11,7 +12,27 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
+      if (pathname.includes('/')) {
+        res.writeHead(400, 'Not allowed')
+        res.end()
+        break
+      }
 
+      unlink(filepath, (err) => {
+        if (err) {
+          if (err.code === 'ENOENT') {
+            res.statusCode = 404
+            res.end()
+          } else {
+            res.statusCode = 500
+            res.end(err.message)
+          }
+        } else {
+          res.statusCode = 200
+          res.end('file has been deleted')
+        }
+
+      })
       break;
 
     default:
